68bb5b83d446d243862526ddb5c07ccb0bf96024
Derek Lin <Derek Lin>
2016-09-05 00:28:42 -0700

    angular 1 and 2 hybrid app

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/index.d.ts"/>
+
+import 'reflect-metadata';
+
+import 'angular';
+import { AppModule } from './app.module';
+import { UpgradeAdapter } from '@angular/upgrade';
+// declare var JQueryStatic : any;
+let upgradeAdapter = new UpgradeAdapter(AppModule);
+angular.module('ng1module', []);
+upgradeAdapter.bootstrap(document.documentElement, ['ng1module']);
---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/require/index.d.ts" />
